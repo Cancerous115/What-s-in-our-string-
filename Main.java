121c4bd4692b0c8ba5749e7fec04590c8418d71a
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-03-05 18:37:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+  public static void main(String[]args){
+    String string1 ="the sky";
+    //Check if text or a words/words are present in our strings
+    System.out.println(string1 .contains("sky"));
+    
+  }
+}
